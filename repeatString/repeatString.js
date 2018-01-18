c8c9abb1c78353b14e005d2e26d663e2ce6b1b4b
Leonard Soai-Van <leo.soaivan@gmail.com>
2018-01-18 18:09:32 -0500

    Add solution to first 3 exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-var repeatString = function() {
+var repeatString = function(string, count) {
+  let result = "";
+  
+  if (count <= 0) {
+    count === 0 ? result : result += "ERROR";
+  } else {
+    while (count) {
+      result += string;
+      count--
+    }
+  }
 
+  return result;
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-var reverseString = function() {
+var reverseString = function(string) {
+  let stringIndex = string.length - 1;
+  let result = "";
 
-}
+  while (stringIndex >= 0) {
+    result += string.charAt(stringIndex);
+    stringIndex--;
+  }
+
+  return result;
+};
 
 module.exports = reverseString
